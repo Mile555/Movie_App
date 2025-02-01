6ded475352c373726ad40a9a0283cf41bc528a84
Mile Stojmenov <milestojmenov998@yahoo.com>
2025-02-01 07:18:17 +0100

    Favorites fix

--- MovieApp/src/pages/Favorites.jsx
+++ MovieApp/src/pages/Favorites.jsx
@@ -1,8 +1,28 @@
-function Favorites(){
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+    
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Favorites Listed</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}></MovieCard>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
     </div>
+  );
 }
 
 export default Favorites;
